70bc77a0f4b473440ea888d71fd5170336308ee0
Lion011095 <lion011095@gmail.con>
2025-06-02 16:47:42 -0400

    broke the code into smaller functions

--- main.py
+++ main.py
@@ -1,45 +1,71 @@
 # Password strength checker that checks how long it will take to crack your password
 import re
 
-if __name__ == '__main__':
 
+def get_password():
     # RegEx for the password checks ( symbols, lower and uppercase letters, number and
     # does nor contain white spaces)
     upper = re.compile(r"[A-Z]")
     lower = re.compile(r"[a-z]")
     num = re.compile(r"\d")
     symbol = re.compile(r"\W")
-    hasSpace = re.compile(r'\s')
+    has_space = re.compile(r'\s')
     valid = False
 
-    print('Welcome to CheckMyPass')
-    print('Here you can check your if password is secure.')
-
     while not valid:
-        charSet = 94
+        charset = 94
         password = input('Enter your password: ')
         print(f'your chosen password is: {password}\n')
         print("Password strengthening tips:")
         valid = True
-        if re.search(hasSpace, password):
+        if re.search(has_space, password):
             print('Your password is invalid because it contains space.')
             print('Try again.')
             valid = False
         else:
             if not re.search(upper, password):
                 print('Your password should contain upper case letters.')
-                charSet -= 26
+                charset -= 26
             if not re.search(lower, password):
                 print('Your password should contain lower case letters.')
-                charSet -= 26
+                charset -= 26
             if not re.search(symbol, password):
                 print('Your password should contain Symbols.')
-                charSet -= 32
+                charset -= 32
             if not re.search(num, password):
                 print('Your password should contain numbers letters.')
-                charSet -= 10
+                charset -= 10
             if len(password) < 12:
                 print('Your password should be at least 12 characters long.')
+            return password, charset
+
+
+# cracking time calculation.
+def estimate_brute_force_time(checking_pass, char_set):
+    pass_length = len(checking_pass)
+    total_combinations = char_set ** pass_length
+    time_in_seconds = total_combinations / 1000000
+    print('\nTime To Crack:')
+    print(f'Total possible combinations: {total_combinations}')
+    if time_in_seconds < 60:
+        print(f"{time_in_seconds:.2f} seconds")
+    elif time_in_seconds < 3600:
+        print(f"{time_in_seconds / 60:.2f} minutes")
+    elif time_in_seconds < 86400:
+        print(f"{time_in_seconds / 3600:.2f} hours")
+    elif time_in_seconds < 31536000:
+        print(f"{time_in_seconds / 86400:.2f} days")
+    else:
+        print(f"{time_in_seconds / 31536000:.2f} years")
+
+
+if __name__ == '__main__':
+
+    print('Welcome to CheckMyPass')
+    print('Here you can check your if password is secure.')
+
+    password, charSet = get_password()
+    estimate_brute_force_time(password, charSet)
 
 # check if password is in a weak password list.
     with open("CommonPasswords.txt") as f:
@@ -49,20 +75,3 @@ if __name__ == '__main__':
             line = n[start:end]
             if password == line:
                 print("Your password is found in a week password list. BE AWARE!!!")
-
-# cracking time calculation.
-    passLength = len(password)
-    totalCombinations = charSet ** passLength
-    timeInSeconds = totalCombinations / 1000000
-    print('\nTime To Crack:')
-    print(f'Total possible combinations: {totalCombinations}')
-    if timeInSeconds < 60:
-        print(f"{timeInSeconds:.2f} seconds")
-    elif timeInSeconds < 3600:
-        print(f"{timeInSeconds / 60:.2f} minutes")
-    elif timeInSeconds < 86400:
-        print(f"{timeInSeconds / 3600:.2f} hours")
-    elif timeInSeconds < 31536000:
-        print(f"{timeInSeconds / 86400:.2f} days")
-    else:
-        print(f"{timeInSeconds / 31536000:.2f} years")
